7f28b9f9b65b87ec2ea3f6f55414ab86aa303d90
gilian58 <113592602+gilian58@users.noreply.github.com>
2022-10-05 23:29:31 +0300

    Create Do while.cpp

---
+++ Do while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    
+    do
+    {
+        cout << "Hello, world!\n";
+        x++;
+    }
+    while (x <= 100);
+    
+    return 0;
+}
